7b0f32acc622c28908a408a290be4673c1dc8645
Amalcs9188 <amalcs645@gmail.com>
2025-08-05 12:58:15 +0530

    * new

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,10 +9,10 @@ import Image from "next/image";
 const Header = () => {
   return (
     <div className=" flex justify-between ">
-      <div className="text-4xl py-3 font-[Poppins] font-semibold">Dashboard</div>
+      <div className="text-4xl py-3 protest-guerrilla-regular font-semibold">Dashboard</div>
       <div>
-        <Card className="">
-          <CardContent className="flex gap-2  items-center">
+        <Card>
+          <CardContent className="flex gap-2 items-center">
             {/* <RxAvatar className=" text-2xl" /> */}
             <span className=' rounded-full border-2 border-white p-1'><Image className='rounded-full  border-white ' src={avatar} width={30} height={30} alt="avatar"/></span>
             <span className="font-semibold text-blue-950">Sanford Richard</span>
